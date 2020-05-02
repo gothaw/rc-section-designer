df7b2ebe840412950e70a0d660e930567a83faf5
gothaw <rad.soltan@gmail.com>
2020-05-02 21:45:21 +0100

    Added setRoot and loadFXML methods in App class, added app title and icon.

--- src/main/java/com/radsoltan/App.java
+++ src/main/java/com/radsoltan/App.java
@@ -1,19 +1,36 @@
 package com.radsoltan;
 
+import com.radsoltan.util.Constants;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class App extends Application {
 
+    private static Scene scene;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/primary.fxml"));
-        primaryStage.setScene(new Scene(root, 300, 300));
+        scene = new Scene(loadFXML("primary"), Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle(Constants.APP_TITLE);
+        primaryStage.getIcons().add(new Image(getClass().getResource("/images/beam.png").toExternalForm()));
         primaryStage.setResizable(false);
         primaryStage.show();
+    }
+
+    public static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/fxml/" + fxml + ".fxml"));
+        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
--- src/main/java/com/radsoltan/util/Constants.java
+++ src/main/java/com/radsoltan/util/Constants.java
@@ -1,5 +1,7 @@
 package com.radsoltan.util;
 
 public class Constants {
-
+    public static final String APP_TITLE = "RC Section Designer";
+    public static final int WINDOW_WIDTH = 500;
+    public static final int WINDOW_HEIGHT = 500;
 }
