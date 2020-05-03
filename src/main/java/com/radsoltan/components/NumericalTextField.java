465005b37ebb7b728549112c0637d1b08c94483e
gothaw <rad.soltan@gmail.com>
2020-05-03 21:47:50 +0100

    Fixed bug in thousands separator for NumericalTextField class.

--- src/main/java/com/radsoltan/components/NumericalTextField.java
+++ src/main/java/com/radsoltan/components/NumericalTextField.java
@@ -1,18 +1,38 @@
 package com.radsoltan.components;
 
+import javafx.scene.control.IndexRange;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
 import javafx.util.converter.NumberStringConverter;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+/**
+ * Text Input FXML component that allows for entering valid floating-point values.
+ * It extends TextField.
+ */
 public class NumericalTextField extends TextField {
 
-    private final String pattern;
+    private final String pattern = "-?(\\d*|0)?(\\.\\d*)?";
 
+    /**
+     * Constructor. Sets text formatter to use NumberStringConverter.
+     * Also sets a regex pattern to find floating point numbers.
+     */
     public NumericalTextField() {
-        this.setTextFormatter(new TextFormatter<>(new NumberStringConverter()));
-        this.pattern = "-?(\\d*|0)?(\\.\\d*)?";
+        DecimalFormat format = new DecimalFormat();
+        format.setGroupingUsed(false);
+        this.setTextFormatter(new TextFormatter<>(new NumberStringConverter(format)));
     }
 
+    /**
+     * Method override.
+     * @param start
+     * @param end
+     * @param text
+     */
     @Override
     public void replaceText(int start, int end, String text) {
         if (start == end){
@@ -22,18 +42,14 @@ public class NumericalTextField extends TextField {
         } else {
             super.replaceText(start, end, text);
         }
+        System.out.println(this.getText());
     }
 
     @Override
-    public void replaceSelection(String replacement) {
-        if(validateReplacement(replacement)){
-            super.replaceSelection(replacement);
+    public void replaceText(IndexRange range, String text) {
+        if(validateReplacement(text)){
+            super.replaceText(range, text);
         }
-    }
-
-    @Override
-    public void paste() {
-        super.paste();
     }
 
     private boolean validateTextInput(String input) {
