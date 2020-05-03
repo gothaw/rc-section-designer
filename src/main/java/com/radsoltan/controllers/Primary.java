d1a4b154613e0a7a182cd229e5cba2579bf301d6
gothaw <rad.soltan@gmail.com>
2020-05-03 21:13:13 +0100

    Added code for NumericalTextField FXML component.

--- src/main/java/com/radsoltan/controllers/Primary.java
+++ src/main/java/com/radsoltan/controllers/Primary.java
@@ -1,5 +1,35 @@
 package com.radsoltan.controllers;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.input.InputMethodEvent;
+
 public class Primary {
+    @FXML
+    private TextField numberField;
+
+    private StringProperty text;
+    private TextFormatter<Double> numerical;
+
+    public Primary(){
+        text = new SimpleStringProperty();
+        System.out.println("hello");
+    }
+
+    public String getText() {
+        return text.get();
+    }
+
+    public StringProperty textProperty() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text.set(text);
+    }
+
 
 }
