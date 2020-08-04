ebeb4a4eea2c2bd31466a63a7b411261e63729b0
gothaw <rad.soltan@gmail.com>
2020-08-04 22:59:47 +0100

    Added method to check for empty fields in primary controller.

--- src/main/java/com/radsoltan/util/Messages.java
+++ src/main/java/com/radsoltan/util/Messages.java
@@ -3,6 +3,7 @@ package com.radsoltan.util;
 public class Messages {
     public static final String ENTER_GEOMETRY = "Enter geometry...";
     public static final String ENTER_REINFORCEMENT = "Enter reinforcement...";
+    public static final String INVALID_ELEMENT_TYPE = "Invalid element type.";
     public static final String INVALID_REDISTRIBUTION_RATIO = "Redistribution ratio must be between 0.7 and 1.0.";
     public static final String INVALID_SLAB_REINFORCEMENT = "Invalid slab reinforcement.";
     public static final String INVALID_SLAB_GEOMETRY = "Invalid slab geometry.";
@@ -12,6 +13,10 @@ public class Messages {
     public static final String SETUP_NOMINAL_COVER = "Please set up nominal cover.";
     public static final String SETUP_CONCRETE_CLASS = "Please set up concrete class.";
     public static final String SETUP_AGGREGATE = "Please set up aggregate size.";
+    public static final String SETUP_ANALYSIS_FORCES = "Please set up analysis forces.";
+    public static final String SETUP_GEOMETRY = "Please set up geometry";
+    public static final String SETUP_REINFORCEMENT = "Please set up reinforcement";
+    public static final String SETUP_DESIGN_PARAMETERS = "Please set up design parameters";
     public static final String SETUP_YIELD_STRENGTH = "Please set up steel yield strength.";
     public static final String SETUP_PARTIAL_FACTORS = "Please set up partial factors for steel and concrete.";
     public static final String WRONG_CONCRETE_CLASS = "Concrete class greater than C50/60. Outside of scope of this software.";
