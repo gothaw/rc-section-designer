c83638192e4f602fee4e6d6cdc143dd184e50396
gothaw <rad.soltan@gmail.com>
2020-08-22 23:41:09 +0100

    Added test for validateslab class.

---
+++ src/test/java/com/radsoltan/model/ValidateSlabTest.java
@@ -0,0 +1,15 @@
+package com.radsoltan.model;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidateSlabTest {
+
+
+    @BeforeAll
+    static void beforeAll() {
+        
+    }
+}
