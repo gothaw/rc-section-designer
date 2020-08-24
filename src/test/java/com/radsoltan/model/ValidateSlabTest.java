13a60f75ee3b9f3a7cc86da58daf2a2b4a60b330
gothaw <rad.soltan@gmail.com>
2020-08-24 21:48:32 +0100

    ValidSlabTest - code refactor - moved the logic to beforeAll.

--- src/test/java/com/radsoltan/model/ValidateSlabTest.java
+++ src/test/java/com/radsoltan/model/ValidateSlabTest.java
@@ -1,7 +1,6 @@
 package com.radsoltan.model;
 
 import com.radsoltan.model.reinforcement.SlabReinforcement;
-import com.radsoltan.util.Constants;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -14,53 +13,67 @@ import static org.junit.jupiter.api.Assertions.*;
 class ValidateSlabTest {
 
     private static DesignParameters designParameters;
+    private static SlabReinforcement validSlabReinforcement;
+    private static int minimumSlabThicknessForValidSpacing;
+    private static SlabReinforcement slabReinforcementWithInvalidVerticalSpacing;
+    private static SlabReinforcement slabReinforcementWithInvalidHorizontalSpacing;
+    private static SlabReinforcement invalidSlabReinforcement;
 
     @BeforeAll
     static void beforeAll() {
         designParameters = new DesignParameters(30, 0, 35, 500, 20, 1.5, 1.15, 0.85, true, true);
-    }
-
-    @Test
-    void noErrorMessagesForValidSlab() {
         List<Integer> topDiameters = new ArrayList<>(List.of(32, 25, 32));
         List<Integer> additionalTopDiameters = new ArrayList<>(List.of(20, 40, 20));
-        List<Integer> topSpacings = new ArrayList<>(List.of(250, 250, 300));
-        List<Integer> topVerticalSpacings = new ArrayList<>(List.of(50, 45, 50));
         List<Integer> bottomDiameters = new ArrayList<>(List.of(32, 25, 32, 20));
         List<Integer> additionalBottomDiameters = new ArrayList<>(List.of(20, 0, 20, 0));
-        List<Integer> bottomSpacings = new ArrayList<>(List.of(400, 400, 300, 400));
-        List<Integer> bottomVerticalSpacings = new ArrayList<>(List.of(50, 45, 50));
-        SlabReinforcement slabReinforcement = new SlabReinforcement(topDiameters, additionalTopDiameters, topSpacings, topVerticalSpacings,
-                bottomDiameters, additionalBottomDiameters, bottomSpacings, bottomVerticalSpacings);
+        List<Integer> validTopSpacings = new ArrayList<>(List.of(250, 250, 300));
+
+        List<Integer> validTopVerticalSpacings = new ArrayList<>(List.of(50, 45));
+        List<Integer> validBottomSpacings = new ArrayList<>(List.of(400, 400, 300, 400));
+        List<Integer> validBottomVerticalSpacings = new ArrayList<>(List.of(50, 45, 50));
+
+        /*List<Integer> invalidTopSpacings = new ArrayList<>(List.of(250, 250, 300));
+        List<Integer> invalidTopVerticalSpacings = new ArrayList<>(List.of(50, 39));
+        List<Integer> invalidBottomSpacings = new ArrayList<>(List.of(400, 400, 300, 400));
+        List<Integer> invalidBottomVerticalSpacings = new ArrayList<>(List.of(50, 45, 50));*/
 
         int topReinforcementHeightSum = IntStream.range(0, topDiameters.size())
                 .map(i -> Math.max(topDiameters.get(i), additionalTopDiameters.get(i)))
                 .reduce(Integer::sum)
                 .orElse(0);
 
-        int topVerticalSpacingSum = topVerticalSpacings.stream().reduce(Integer::sum).orElse(0);
-
         int bottomReinforcementHeightSum = IntStream.range(0, bottomDiameters.size())
                 .map(i -> Math.max(bottomDiameters.get(i), additionalBottomDiameters.get(i)))
                 .reduce(Integer::sum)
                 .orElse(0);
 
-        int bottomVerticalSpacingSum = bottomVerticalSpacings.stream().reduce(Integer::sum).orElse(0);
-
         int distanceBetweenTopAndBottomReinforcement = IntStream.of(20, designParameters.getAggregateSize() + 5,
                 topDiameters.get(topDiameters.size() - 1), additionalTopDiameters.get(additionalTopDiameters.size() - 1),
                 bottomDiameters.get(bottomDiameters.size() - 1), additionalBottomDiameters.get(additionalBottomDiameters.size() - 1)).max().orElse(0);
 
-        int thickness = designParameters.getNominalCoverTop() + topReinforcementHeightSum + topVerticalSpacingSum + distanceBetweenTopAndBottomReinforcement + bottomReinforcementHeightSum + bottomVerticalSpacingSum + designParameters.getNominalCoverBottom();
+        int validTopVerticalSpacingSum = validTopVerticalSpacings.stream().reduce(Integer::sum).orElse(0);
+        int validBottomVerticalSpacingSum = validBottomVerticalSpacings.stream().reduce(Integer::sum).orElse(0);
 
-        ValidateSlab validateSlab = new ValidateSlab(thickness, slabReinforcement, designParameters);
+        minimumSlabThicknessForValidSpacing = designParameters.getNominalCoverTop() + topReinforcementHeightSum + validTopVerticalSpacingSum + distanceBetweenTopAndBottomReinforcement + bottomReinforcementHeightSum + validBottomVerticalSpacingSum + designParameters.getNominalCoverBottom();
 
-        assertEquals(validateSlab.getValidationMessages(), new ArrayList<>());
+        validSlabReinforcement = new SlabReinforcement(topDiameters, additionalTopDiameters, validTopSpacings, validTopVerticalSpacings,
+                bottomDiameters, additionalBottomDiameters, validBottomSpacings, validBottomVerticalSpacings);
+    }
+
+    @Test
+    void noErrorMessagesForValidSlab() {
+        ValidateSlab validateSlab = new ValidateSlab(minimumSlabThicknessForValidSpacing, validSlabReinforcement, designParameters);
+
+        assertEquals(new ArrayList<>(), validateSlab.getValidationMessages());
     }
 
     @Test
     void errorMessageIsAddedWhenSlabThicknessIsInvalid() {
+        int invalidSlabThickness = minimumSlabThicknessForValidSpacing - 10;
+        ValidateSlab validateSlab = new ValidateSlab(invalidSlabThickness, validSlabReinforcement, designParameters);
 
+        assertEquals(1, validateSlab.getValidationMessages().size());
+        assertEquals("Invalid slab thickness. Minimum slab thickness: 550 mm.", validateSlab.getValidationMessages().get(0));
     }
 
     @Test
