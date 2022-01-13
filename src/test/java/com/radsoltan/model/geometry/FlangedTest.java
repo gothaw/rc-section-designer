ddd671fc139d61068bd2205c62f3cad9b1cb3cfe
gothaw <rad.soltan@gmail.com>
2022-01-13 22:43:12 +0000

    Added tests for Flanged interface

--- src/test/java/com/radsoltan/model/geometry/FlangedTest.java
+++ src/test/java/com/radsoltan/model/geometry/FlangedTest.java
@@ -1,9 +1,10 @@
 package com.radsoltan.model.geometry;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.spy;
 
 class FlangedTest {
 
@@ -11,6 +12,33 @@ class FlangedTest {
 
     @BeforeAll
     static void beforeAll() {
-        flanged = mock(Flanged.class);
+        flanged = spy(Flanged.class);
+    }
+
+    @Test
+    void factorKIsCalculatedCorrectlyForSmallSize() {
+        int size = 200;
+
+        double kFactor = flanged.getFactorForNonUniformSelfEquilibratingStressesForWebOrFlange(size);
+
+        assertEquals(1.0, kFactor);
+    }
+
+    @Test
+    void factorKIsCalculatedCorrectlyForIntermediateSize() {
+        int size = 600;
+
+        double kFactor = flanged.getFactorForNonUniformSelfEquilibratingStressesForWebOrFlange(size);
+
+        assertEquals(0.79, kFactor);
+    }
+
+    @Test
+    void factorKIsCalculatedCorrectlyForLargeSize() {
+        int size = 1000;
+
+        double kFactor = flanged.getFactorForNonUniformSelfEquilibratingStressesForWebOrFlange(size);
+
+        assertEquals(0.65, kFactor);
     }
 }
