8a5af70c56c8e2899d3aae8ce7c7718a2c47b99b
gothaw <rad.soltan@gmail.com>
2022-01-24 21:16:49 +0000

    Added docs for Project

--- src/main/java/com/radsoltan/model/Project.java
+++ src/main/java/com/radsoltan/model/Project.java
@@ -6,6 +6,14 @@ import com.radsoltan.model.reinforcement.Reinforcement;
 import com.radsoltan.model.reinforcement.SlabReinforcement;
 import com.radsoltan.util.Messages;
 
+/**
+ * Class is used to represent project calculations. It includes values from fields shown in primary controller along with:
+ * - Geometry
+ * - Reinforcement
+ * - Design Parameters
+ * - Concrete type
+ * It uses all that information to calculate flexural and shear capacity and crack widths (if applicable). The class uses singleton pattern.
+ */
 public class Project {
     private String name;
     private String id;
@@ -32,6 +40,11 @@ public class Project {
 
     private static Project project;
 
+    /**
+     * Gets project instance. Singleton pattern.
+     *
+     * @return project instance
+     */
     public static Project getInstance() {
         if (project == null) {
             project = new Project();
@@ -39,6 +52,9 @@ public class Project {
         return project;
     }
 
+    /**
+     * Handler for calculate button. It invokes method that depends on structural element type.
+     */
     public void calculate() {
         switch (elementType.toLowerCase()) {
             case "slab":
@@ -52,6 +68,10 @@ public class Project {
         }
     }
 
+    /**
+     * Method used to calculate slab project.
+     * It calculates bending capacity and crack widths based on forces and parameters provided.
+     */
     private void calculateSlabProject() {
         if (!(geometry.getSection() instanceof SlabStrip)) {
             throw new IllegalArgumentException(Messages.INVALID_SLAB_GEOMETRY);
@@ -59,12 +79,16 @@ public class Project {
         if (!(reinforcement instanceof SlabReinforcement)) {
             throw new IllegalArgumentException(Messages.INVALID_SLAB_REINFORCEMENT);
         }
+        // Getting slab section and slab reinforcement
         SlabStrip slabStrip = (SlabStrip) geometry.getSection();
         SlabReinforcement slabReinforcement = (SlabReinforcement) reinforcement;
+        // Getting moment values
         double UlsMomentValue = Double.parseDouble(UlsMoment);
         double SlsMomentValue = Double.parseDouble(SlsMoment);
+        // Instantiating new slab
         Slab slab = new Slab(UlsMomentValue, SlsMomentValue, slabStrip, concrete, slabReinforcement, designParameters);
         try {
+            // Calculating bending capacity
             slab.calculateBendingCapacity();
             flexureCapacity = slab.getBendingCapacity();
             flexureCapacityCheckMessage = (Math.abs(UlsMomentValue) <= flexureCapacity) ?
@@ -78,143 +102,313 @@ public class Project {
         }
         if (designParameters.isIncludeCrackingCalculations()) {
             // TODO: 11/08/2020 Wrap in try catch and implement method
+            // Calculating crack widths
             slab.calculateCracks();
 
         }
     }
 
+    /**
+     * Method used to calculate beam project.
+     * It calculates bending and shear capacity and crack widths based on forces and parameters provided.
+     */
     private void calculateBeamProject() {
         // TODO: 11/08/2020 Implement method
     }
 
+    /**
+     * Getter for project name.
+     *
+     * @return project name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Setter for project name.
+     *
+     * @param name project name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Getter for project Id.
+     *
+     * @return project Id
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Setter for project Id.
+     *
+     * @param id project Id
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /**
+     * Getter for project description.
+     *
+     * @return project description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Setter for project description.
+     *
+     * @param description project description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Getter for project author.
+     *
+     * @return project author
+     */
     public String getAuthor() {
         return author;
     }
 
+    /**
+     * Setter for project author.
+     *
+     * @param author project author
+     */
     public void setAuthor(String author) {
         this.author = author;
     }
 
+    /**
+     * Getter for ULS moment.
+     *
+     * @return ULS moment
+     */
     public String getUlsMoment() {
         return UlsMoment;
     }
 
+    /**
+     * Setter for ULS moment.
+     *
+     * @param ulsMoment moment
+     */
     public void setUlsMoment(String ulsMoment) {
         this.UlsMoment = ulsMoment;
     }
 
+    /**
+     * Getter for SLS moment.
+     *
+     * @return SLS moment
+     */
     public String getSlsMoment() {
         return SlsMoment;
     }
 
+    /**
+     * Setter for SLS moment.
+     *
+     * @param slsMoment moment
+     */
     public void setSlsMoment(String slsMoment) {
         this.SlsMoment = slsMoment;
     }
 
+    /**
+     * Getter for ULS shear force.
+     *
+     * @return ULS shear
+     */
     public String getUlsShear() {
         return UlsShear;
     }
 
+    /**
+     * Setter for ULS shear force.
+     *
+     * @param ulsShear shear force
+     */
     public void setUlsShear(String ulsShear) {
         this.UlsShear = ulsShear;
     }
 
+    /**
+     * Getter for element type.
+     *
+     * @return element type
+     */
     public String getElementType() {
         return elementType;
     }
 
+    /**
+     * Setter for element type.
+     *
+     * @param elementType element type
+     */
     public void setElementType(String elementType) {
         this.elementType = elementType;
     }
 
+    /**
+     * Getter for structural element geometry.
+     *
+     * @return geometry
+     */
     public Geometry getGeometry() {
         return geometry;
     }
 
+    /**
+     * Setter for structural element geometry.
+     *
+     * @param geometry geometry object
+     */
     public void setGeometry(Geometry geometry) {
         this.geometry = geometry;
     }
 
+    /**
+     * Getter for structural element reinforcement.
+     *
+     * @return reinforcement
+     */
     public Reinforcement getReinforcement() {
         return reinforcement;
     }
 
+    /**
+     * Setter for structural element reinforcement.
+     *
+     * @param reinforcement reinforcement object
+     */
     public void setReinforcement(Reinforcement reinforcement) {
         this.reinforcement = reinforcement;
     }
 
+    /**
+     * Getter for design parameters used in calculations.
+     *
+     * @return design parameters
+     */
     public DesignParameters getDesignParameters() {
         return designParameters;
     }
 
+    /**
+     * Setter for design parameters used in calculations.
+     *
+     * @param designParameters design parameters object
+     */
     public void setDesignParameters(DesignParameters designParameters) {
         this.designParameters = designParameters;
     }
 
+    /**
+     * Getter for concrete type.
+     *
+     * @return concrete
+     */
     public Concrete getConcrete() {
         return concrete;
     }
 
+    /**
+     * Setter for concrete type.
+     *
+     * @param concrete concrete enum
+     */
     public void setConcrete(Concrete concrete) {
         this.concrete = concrete;
     }
 
+    /**
+     * Getter for bending capacity.
+     *
+     * @return bending capacity
+     */
     public double getFlexureCapacity() {
         return flexureCapacity;
     }
 
+    /**
+     * Getter for shear capacity.
+     *
+     * @return shear capacity
+     */
     public double getShearCapacity() {
         return shearCapacity;
     }
 
+    /**
+     * Getter for calculated crack widths.
+     *
+     * @return crack widths
+     */
     public double getCrackWidths() {
         return crackWidths;
     }
 
+    /**
+     * Getter for bending capacity calculation message.
+     *
+     * @return calculation message
+     */
     public String getFlexureCapacityCheckMessage() {
         return flexureCapacityCheckMessage;
     }
 
+    /**
+     * Getter for shear capacity calculation message.
+     *
+     * @return calculation message
+     */
     public String getShearCapacityCheckMessage() {
         return shearCapacityCheckMessage;
     }
 
+    /**
+     * Getter for cracking calculation message.
+     *
+     * @return calculation message
+     */
     public String getCrackingCheckMessage() {
         return crackingCheckMessage;
     }
 
+    /**
+     * Getter for additional calculation message for bending calculations.
+     *
+     * @return additional calculation message
+     */
     public String getFlexureResultsAdditionalMessage() {
         return flexureResultsAdditionalMessage;
     }
 
+    /**
+     * Getter for additional calculation message for shear calculations.
+     *
+     * @return additional calculation message
+     */
     public String getShearResultsAdditionalMessage() {
         return shearResultsAdditionalMessage;
     }
 
+    /**
+     * Getter for additional calculation message for crack width calculations.
+     *
+     * @return additional calculation message
+     */
     public String getCrackingResultsAdditionalMessage() {
         return crackingResultsAdditionalMessage;
     }
