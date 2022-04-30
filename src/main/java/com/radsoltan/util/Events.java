3b3e1b42b8cc5d87cee492be795501614bd41833
gothaw <rad.soltan@gmail.com>
2022-04-30 23:33:05 +0100

    Added save as event

--- src/main/java/com/radsoltan/util/Events.java
+++ src/main/java/com/radsoltan/util/Events.java
@@ -7,4 +7,5 @@ public class Events {
     public static final String NEW_FILE = "new_file";
     public static final String OPEN_FILE = "open_file";
     public static final String SAVE_FILE = "save_file";
+    public static final String SAVE_AS_FILE = "save_as_file";
 }
