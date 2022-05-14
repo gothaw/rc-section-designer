06ebc8f2ca27092a08ae2a0209d57cfad3f87488
gothaw <rad.soltan@gmail.com>
2022-05-14 23:11:45 +0100

    Finished about controller.

--- src/main/java/com/radsoltan/controllers/About.java
+++ src/main/java/com/radsoltan/controllers/About.java
@@ -1,11 +1,48 @@
 package com.radsoltan.controllers;
 
-import java.util.List;
+import com.radsoltan.App;
+import com.radsoltan.constants.UIText;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
-public class About extends Controller {
+import java.time.LocalDate;
 
-    @Override
-    protected List<String> getValidationMessagesForEmptyFields() {
-        return null;
+/**
+ * Controller for the about menu item window.
+ */
+public class About {
+
+    @FXML
+    public Label buildLabel;
+    @FXML
+    public Label appTitle;
+    @FXML
+    public Label year;
+    @FXML
+    public Label author;
+
+    /**
+     * Initializes fields.
+     */
+    @FXML
+    public void initialize() {
+        appTitle.setText(UIText.APP_TITLE);
+        author.setText(UIText.APP_AUTHOR);
+        year.setText(Integer.toString(LocalDate.now().getYear()));
+
+        String version = getClass().getPackage().getImplementationVersion();
+
+        buildLabel.setText(String.format("Build %s", version == null ? "DEV" : version));
+    }
+
+    /**
+     * Handler for the ok button. Closes the window.
+     *
+     * @param actionEvent button click event
+     */
+    public void close(ActionEvent actionEvent) {
+        App.getSecondaryWindow().getStage().close();
+        App.setSecondaryWindow(null);
     }
 }
