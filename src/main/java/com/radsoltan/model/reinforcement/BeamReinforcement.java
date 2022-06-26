23a41256b31954ed7a53d635f0ad96cba569ac1c
gothaw <rad.soltan@gmail.com>
2022-06-26 13:59:34 +0100

    Adding documentation. Refactoring getDistanceFromCentreOfEachBarToEdge.

--- src/main/java/com/radsoltan/model/reinforcement/BeamReinforcement.java
+++ src/main/java/com/radsoltan/model/reinforcement/BeamReinforcement.java
@@ -4,56 +4,78 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+/**
+ * Model for beam reinforcement. It is used to create an object that describes beam reinforcement that can be used in structural calculations.
+ * In addition, allows for beam reinforcement on graphics context.
+ */
 public class BeamReinforcement extends Reinforcement {
 
     private final List<List<Integer>> topDiameters;
-    private List<Integer> topVerticalSpacings;
+    private final List<Integer> topVerticalSpacings;
     private final List<List<Integer>> bottomDiameters;
-    private List<Integer> bottomVerticalSpacings;
+    private final List<Integer> bottomVerticalSpacings;
     private final ShearLinks shearLinks;
-    private int numberOfBarsInSlab;
-    private boolean isReinforcementInSlabSymmetrical;
 
+    /**
+     * Constructor. Used in structural calculations.
+     * <p>
+     * The reinforcement is described using following lists:
+     * <p>
+     * - topDiameters/bottomDiameters: these are lists of lists of integers, the length of the outer list corresponds to number of rows - top/bottom.
+     * The inner lists include reinforcement diameters for each bar in given row.
+     * For example, reinforcement object with topDiameters of [[25, 20, 25, 20, 25], [16, 12, 16], [10, 8, 8, 10]] has 3 rows:
+     * 1st row: 3 x 25 mm bars, 2 x 20 mm bars (5 in total)
+     * 2nd row: 2 x 16 mm bars, 1 x 16 mm bar (3 in total)
+     * 3rd row: 2 x 10 mm bars, 2 x 8 mm bar (4 in total)
+     * <p>
+     * - topVerticalSpacings/bottomVerticalSpacings: these include clear vertical spacings between subsequent row for top/bottom face.
+     * For instance, if reinforcement object has topDiameters of [[20, 16, 16, 20], [16, 16], [10, 10]] and topVerticalSpacings of [75, 50] then the top reinforcement has 3 rows:
+     * 1st row: 2 x 20 mm bars, 2 x 16 mm bars (4 in total)
+     * 2nd row: 2 x 16 mm bars (2 in total)
+     * 3rd row: 2 x 10 mm bars (2 in total)
+     * The clear distance between 1st and 2nd row is 75 mm and between 2nd and 3rd is 50 mm.
+     *
+     * <p>
+     * The reinforcement also includes a ShearLinks object that describes shear reinforcement.
+     *
+     * @param topDiameters           contains top bar diameters in mm
+     * @param topVerticalSpacings    clear vertical spacings between top rows in mm
+     * @param bottomDiameters        contains bottom bar diameters in mm
+     * @param bottomVerticalSpacings clear vertical spacings between bottom rows in mm
+     * @param shearLinks             shear links object
+     */
     public BeamReinforcement(List<List<Integer>> topDiameters,
-            List<Integer> topVerticalSpacings,
-            List<List<Integer>> bottomDiameters,
-            List<Integer> bottomVerticalSpacings,
-            ShearLinks shearLinks,
-            int numberOfBarsInSlab,
-            boolean isReinforcementInSlabSymmetrical) {
+                             List<Integer> topVerticalSpacings,
+                             List<List<Integer>> bottomDiameters,
+                             List<Integer> bottomVerticalSpacings,
+                             ShearLinks shearLinks) {
 
         this.topDiameters = topDiameters;
         this.topVerticalSpacings = topVerticalSpacings;
         this.bottomDiameters = bottomDiameters;
         this.bottomVerticalSpacings = bottomVerticalSpacings;
         this.shearLinks = shearLinks;
-        this.numberOfBarsInSlab = numberOfBarsInSlab;
-        this.isReinforcementInSlabSymmetrical = isReinforcementInSlabSymmetrical;
     }
 
-    public BeamReinforcement(List<List<Integer>> topDiameters,
-                             List<Integer> topVerticalSpacings,
-                             List<List<Integer>> bottomDiameters,
-                             List<Integer> bottomVerticalSpacings,
-                             ShearLinks shearLinks) {
-        this(topDiameters, topVerticalSpacings, bottomDiameters, bottomVerticalSpacings, shearLinks, 0, false);
+    /**
+     * Method used to get a list of rows, each row is list that stores area (in mm2) of each bar in that row.
+     *
+     * @param diameters list of reinforcement rows, each row is a list with bar diameters in mm
+     * @return List of reinforcement rows, each row is list that stores area (in mm2) of each bar in that row
+     */
+    private List<List<Double>> getAreaOfReinforcementBars(List<List<Integer>> diameters) {
+        return diameters.stream()
+                .map(row -> row.stream()
+                        .map(diameter -> diameter * diameter * Math.PI * 0.25)
+                        .collect(Collectors.toList()))
+                .collect(Collectors.toList());
     }
 
-    public BeamReinforcement(List<Integer> topReinforcementRow, List<Integer> bottomReinforcementRow, ShearLinks shearLinks) {
-
-        List<List<Integer>> topDiameters = new ArrayList<>();
-        topDiameters.add(new ArrayList<>());
-        topDiameters.get(0).addAll(topReinforcementRow);
-        this.topDiameters = topDiameters;
-
-        List<List<Integer>> bottomDiameters = new ArrayList<>();
-        bottomDiameters.add(new ArrayList<>());
-        bottomDiameters.get(0).addAll(bottomReinforcementRow);
-        this.bottomDiameters = bottomDiameters;
-
-        this.shearLinks = shearLinks;
-    }
-
+    /**
+     * Calculates total area of top reinforcement.
+     *
+     * @return Total area of top reinforcement in mm2
+     */
     @Override
     public double getTotalAreaOfTopReinforcement() {
         return getAreaOfReinforcementBars(topDiameters).stream()
@@ -62,6 +84,108 @@ public class BeamReinforcement extends Reinforcement {
                 .sum();
     }
 
+    /**
+     * Calculates total area of bottom reinforcement.
+     *
+     * @return Total area of bottom reinforcement in mm2
+     */
+    @Override
+    public double getTotalAreaOfBottomReinforcement() {
+        return getAreaOfReinforcementBars(bottomDiameters).stream()
+                .flatMap(Collection::stream)
+                .mapToDouble(Double::doubleValue)
+                .sum();
+    }
+
+    /**
+     * Calculates distances from each bar to the beam edge - top/bottom.
+     * <p>
+     * First row bars are considered separately as their distance to the edge is: nominalCover + shearLinkDiameter + 0.5 * barDiameter.
+     * This is because first row bars are tied to shear links and don't lie on one horizontal centre line (unless they have the same diameters).
+     * <p>
+     * Subsequent rows are placed on one horizontal centre line and are separated from other rows by a clear vertical spacing.
+     * <p>
+     * The result is a list of lists, the outer list size corresponds to number of rows.
+     * The values of inner lists are distances from centre of a bar to the beam edge. For example a result of [[50, 40, 50], [125, 125, 125], [185, 185]] means that:
+     * - 1st row: 3 bars in total, 2 of them are 50 mm from the edge and one is 40 mm from the edge
+     * - 2nd row: 3 bars in total, all of them are 125 mm from the beam's edge
+     * - 3rd row: 2 bars in total, all of them are 185 mm from the beam's edge
+     *
+     * @param diameters list of reinforcement rows, each row is a list with bar diameters in mm
+     * @param clearVerticalSpacings    clear vertical spacings between rows in mm
+     * @param nominalCover nominal cover for given beam's edge
+     *
+     * @return List of reinforcement rows, each row is list that stores distances from centre of each bar to the beam's edge
+     */
+    private List<List<Double>> getDistanceFromCentreOfEachBarToEdge(List<List<Integer>> diameters, List<Integer> clearVerticalSpacings, int nominalCover) {
+        List<List<Double>> distanceFromCentroidOfEachBarToEdge = new ArrayList<>();
+
+        // Distances for first row bars
+        List<Double> distanceForBarsInFirstRow = diameters.get(0).stream()
+                .map(diameter -> diameter * 0.5 + nominalCover + shearLinks.getDiameter())
+                .collect(Collectors.toList());
+
+        distanceFromCentroidOfEachBarToEdge.add(distanceForBarsInFirstRow);
+
+        // Distances for subsequent rows
+        double largestDistanceForFirstRow = Collections.max(distanceForBarsInFirstRow);
+
+        // Max diameters for each row
+        List<Integer> maxDiametersForEachRow = diameters.stream()
+                .map(Collections::max)
+                .collect(Collectors.toList());
+
+        // Converting clear spacings to vertical spacings
+        List<Double> verticalSpacings = IntStream
+                .range(0, clearVerticalSpacings.size())
+                .mapToObj(i -> clearVerticalSpacings.get(i) + 0.5 * maxDiametersForEachRow.get(i) + 0.5 * maxDiametersForEachRow.get(i + 1))
+                .collect(Collectors.toList());
+
+        // Calculating distances from centroid of each row to edge
+        List<Double> distancesForSubsequentRows = IntStream
+                .range(0, verticalSpacings.size())
+                .mapToObj(i -> largestDistanceForFirstRow + verticalSpacings.get(i) + verticalSpacings.stream().limit(i).reduce(0.0, Double::sum))
+                .collect(Collectors.toList());
+
+        // Starting from the second row
+        List<List<Double>> distanceForBarsInSubsequentRows = IntStream.range(1, diameters.size())
+                .mapToObj(i -> IntStream
+                        .range(0, diameters.get(i).size())
+                        .mapToObj(distance -> distancesForSubsequentRows.get(i))
+                        .collect(Collectors.toList()))
+                .collect(Collectors.toList());
+
+        distanceFromCentroidOfEachBarToEdge.addAll(distanceForBarsInSubsequentRows);
+
+        return distanceFromCentroidOfEachBarToEdge;
+    }
+
+    /**
+     *
+     * @param areasOfReinforcementBars
+     * @param diameters
+     * @param verticalBarSpacing
+     * @param nominalCover
+     * @param isTopReinforcement
+     * @return
+     */
+    public List<List<Double>> getFirstMomentOfAreaForReinforcementBars(List<List<Double>> areasOfReinforcementBars, List<List<Integer>> diameters, List<Integer> verticalBarSpacing, int nominalCover, boolean isTopReinforcement) {
+
+        List<List<Double>> distanceFromCentreOfEachBarToEdge = getDistanceFromCentreOfEachBarToEdge(diameters, verticalBarSpacing, nominalCover);
+
+        return IntStream
+                .range(0, distanceFromCentreOfEachBarToEdge.size())
+                .mapToObj(i -> IntStream
+                        .range(0, distanceFromCentreOfEachBarToEdge.get(i).size())
+                        .mapToObj(j -> distanceFromCentreOfEachBarToEdge.get(i).get(j) * areasOfReinforcementBars.get(i).get(j))
+                        .collect(Collectors.toList()))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * @param nominalCoverTop nominal cover for the top face of the element in mm
+     * @return
+     */
     @Override
     public double getCentroidOfTopReinforcement(int nominalCoverTop) {
         List<List<Double>> areaOfTopBars = getAreaOfReinforcementBars(topDiameters);
@@ -76,14 +200,11 @@ public class BeamReinforcement extends Reinforcement {
         return sumOfFirstMomentsOfArea / sumOfAreas;
     }
 
-    @Override
-    public double getTotalAreaOfBottomReinforcement() {
-        return getAreaOfReinforcementBars(bottomDiameters).stream()
-                .flatMap(Collection::stream)
-                .mapToDouble(Double::doubleValue)
-                .sum();
-    }
-
+    /**
+     *
+     * @param nominalCoverBottom nominal cover fore the bottom face of the element in mm
+     * @return
+     */
     @Override
     public double getCentroidOfBottomReinforcement(int nominalCoverBottom) {
         List<List<Double>> areaOfBottomBars = getAreaOfReinforcementBars(bottomDiameters);
@@ -112,78 +233,6 @@ public class BeamReinforcement extends Reinforcement {
     public String getDescription() {
         // TODO: 02/08/2020
         return null;
-    }
-
-    public List<List<Double>> getAreaOfReinforcementBars(List<List<Integer>> reinforcement) {
-        return reinforcement.stream()
-                .map(row -> row.stream()
-                        .map(diameter -> diameter * diameter * Math.PI * 0.25)
-                        .collect(Collectors.toList()))
-                .collect(Collectors.toList());
-    }
-
-    // TODO: 10/06/2020 Encapsulation
-    public List<List<Double>> getDistanceFromCentreOfEachBarToEdge(List<List<Integer>> reinforcement, List<Integer> verticalBarSpacing, int nominalCover, boolean isTopReinforcement) {
-
-        List<List<Double>> distanceFromCentroidOfEachBarToEdge = new ArrayList<>();
-
-        // Distance For First Row
-        List<Double> distanceForBarsInFirstRow = reinforcement.get(0).stream()
-                .map(diameter -> diameter * 0.5 + nominalCover + shearLinks.getDiameter())
-                .collect(Collectors.toList());
-
-        distanceFromCentroidOfEachBarToEdge.add(distanceForBarsInFirstRow);
-
-        // Distance For Subsequent Rows
-        double largestDistanceToFirstRowBar = getMaxDistanceFromFirstRowRebarToEdgeExcludingSlabRebar(distanceForBarsInFirstRow, isTopReinforcement);
-
-        List<List<Integer>> barsInSubsequentRows = new ArrayList<>(reinforcement);
-        barsInSubsequentRows.remove(0);
-
-        List<Double> distanceForSubsequentRows = IntStream
-                .range(0, verticalBarSpacing.size())
-                .mapToObj(i -> verticalBarSpacing.get(i) + largestDistanceToFirstRowBar + verticalBarSpacing.stream()
-                        .limit(i)
-                        .reduce(0, Integer::sum))
-                .collect(Collectors.toList());
-
-        List<List<Double>> distanceForBarsInSubsequentRows = IntStream.range(0, barsInSubsequentRows.size())
-                .mapToObj(i -> IntStream
-                        .range(0, barsInSubsequentRows.get(i).size())
-                        .mapToObj(distance -> distanceForSubsequentRows.get(i))
-                        .collect(Collectors.toList()))
-                .collect(Collectors.toList());
-
-        distanceFromCentroidOfEachBarToEdge.addAll(distanceForBarsInSubsequentRows);
-
-        return distanceFromCentroidOfEachBarToEdge;
-    }
-
-    private double getMaxDistanceFromFirstRowRebarToEdgeExcludingSlabRebar(List<Double> distanceForBarsInFirstRow, boolean isTopReinforcement) {
-        if (isTopReinforcement) {
-            int numberOfBarsToBeExcluded = (isReinforcementInSlabSymmetrical) ? numberOfBarsInSlab / 2 : numberOfBarsInSlab;
-            List<Double> distanceForBarsInFirstRowExcludingSlab = IntStream
-                    .range(numberOfBarsToBeExcluded, distanceForBarsInFirstRow.size() - numberOfBarsToBeExcluded)
-                    .mapToObj(distanceForBarsInFirstRow::get)
-                    .collect(Collectors.toList());
-
-            return Collections.max(distanceForBarsInFirstRowExcludingSlab);
-        } else {
-            return Collections.max(distanceForBarsInFirstRow);
-        }
-    }
-
-    public List<List<Double>> getFirstMomentOfAreaForReinforcementBars(List<List<Double>> areaOfReinforcementBars, List<List<Integer>> reinforcement, List<Integer> verticalBarSpacing, int nominalCover, boolean isTopReinforcement) {
-
-        List<List<Double>> distanceFromCentreOfEachBarToEdge = getDistanceFromCentreOfEachBarToEdge(reinforcement, verticalBarSpacing, nominalCover, isTopReinforcement);
-
-        return IntStream
-                .range(0, distanceFromCentreOfEachBarToEdge.size())
-                .mapToObj(i -> IntStream
-                        .range(0, distanceFromCentreOfEachBarToEdge.get(i).size())
-                        .mapToObj(j -> distanceFromCentreOfEachBarToEdge.get(i).get(j) * areaOfReinforcementBars.get(i).get(j))
-                        .collect(Collectors.toList()))
-                .collect(Collectors.toList());
     }
 
     public List<List<Integer>> getTopReinforcement() {
