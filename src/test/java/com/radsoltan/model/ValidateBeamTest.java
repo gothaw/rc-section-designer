277727f70141a9dc8cab4a38ff721f8352b38e23
gothaw <rad.soltan@gmail.com>
2022-08-10 23:05:04 +0100

    Added template for ValidateBeamTest.java test cases

--- src/test/java/com/radsoltan/model/ValidateBeamTest.java
+++ src/test/java/com/radsoltan/model/ValidateBeamTest.java
@@ -1,12 +1,49 @@
 package com.radsoltan.model;
 
+import com.radsoltan.model.geometry.Geometry;
+import com.radsoltan.model.reinforcement.BeamReinforcement;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ValidateBeamTest {
+
+    private static Geometry geometry;
+    private static DesignParameters designParameters;
+    private static BeamReinforcement validBeamReinforcement;
+    private static int minimumBeamDepthForValidSpacing;
+    private static BeamReinforcement beamReinforcementWithInvalidHorizontalSpacing;
+    private static BeamReinforcement beamReinforcementWithInvalidVerticalSpacing;
+    private static BeamReinforcement invalidBeamReinforcement;
+
+    @BeforeAll
+    static void beforeAll() {
+        designParameters = new DesignParameters(30, 25, 35, 500, 20, 1.5, 1.15, 0.85, true, true, 0.3);
+
+    }
+
+
     @Test
-    void name() {
-        // TODO: 30/08/2020 Implement tests for ValidateBeam
+    void noErrorMessagesForValidBeam() {
+
+    }
+
+    @Test
+    void errorMessageIsAddedWhenBeamDepthIsInvalid() {
+
+    }
+
+    @Test
+    void errorMessageIsAddedWhenHorizontalBarSpacingIsInvalid() {
+
+    }
+
+    @Test
+    void errorMessageIsAddedWhenVerticalSpacingBetweenRowsIsInvalid() {
+
+    }
+
+    @Test
+    void errorMessageIsAddedWhenBeamReinforcementIsInvalid() {
+
     }
 }
