7c92099bb6ced9e92a532a86e4b17ea1a67cc735
gothaw <rad.soltan@gmail.com>
2022-09-17 11:34:35 +0100

    Added template for ProjectTest

---
+++ src/test/java/com/radsoltan/model/ProjectTest.java
@@ -0,0 +1,27 @@
+package com.radsoltan.model;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class ProjectTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        // TODO: 17/09/2022 Add these
+    }
+
+    @Test
+    void shouldCalculateSlabProject() {
+
+    }
+
+    @Test
+    void shouldCalculateBeamProject() {
+
+    }
+
+    @Test
+    void shouldResetProject() {
+
+    }
+}
